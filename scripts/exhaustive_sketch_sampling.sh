3b20c8b8634e72356afc6cb4e89b80f11fa0800c
MangoShip <mingun0108@gmail.com>
2026-05-20 00:46:17 -0700

    Added automatic script for exhaustive sketch sampling

---
+++ scripts/exhaustive_sketch_sampling.sh
@@ -0,0 +1,74 @@
+#
+# Name: exhaustive_sketch_sampling.sh
+#
+# Exhaustive sketch sampling experiment
+#   for showing that sketch can sample log(V) spanning forests.
+#
+
+if [[ $# -lt 7 ]]; then
+  echo "ERROR: Invalid Arguments!"
+  echo "USAGE: exec_dir datasets_dir results_dir workers readers num_trials seed"
+  echo "----------------------------------------------------------------"
+  echo "exec_dir:              Directory where the executable is placed"
+  echo "datasets_dir:          Directory where datasets are placed"
+  echo "results_dir:           Directory where csv file should be placed"
+  echo "workers:               Number of graph workers"
+  echo "readers:               Number of graph readers"
+  echo "num_trials:            Number of trials"
+  echo "seed:                  Sketch Seed (Optinal. Default to random seed)"
+  exit 1
+fi
+
+exec_dir=$1
+datasets_dir=$2
+result_dir=$3
+workers=$4
+readers=$5
+num_trials=$6
+
+if [[ -n "$7" ]]; then
+  seed="$7"
+else
+  seed=""
+fi
+
+# Datasets
+kron_graphs=("kron_13_stream_binary" "kron_15_stream_binary"
+             "kron_16_stream_binary" "kron_17_stream_binary")
+
+sparse_graphs=("ca_citeseer_stream_binary" "google_plus_stream_binary"
+               "web_uk_stream_binary") # Excluding p2p and rec_amazon because |V| too low
+
+ktree_graphs=("ktree_13_2048_stream_binary_shuffled" 
+              "ktree_15_8192_stream_binary_shuffled"
+              "ktree_16_16384_stream_binary_shuffled" 
+              "ktree_17_32768_stream_binary_shuffled")
+
+out_file=runtime_results.csv
+
+for stream_name in "${kron_graphs[@]}"
+do
+  > ${out_file}
+  echo "size,samples" > ${out_file}
+  ${exec_dir}/spanning_forest_extract ${datasets_dir}/kron/${stream_name} $workers $readers $num_trials $seed
+  mv ${out_file} $result_dir/exhaustive_${stream_name}.csv
+done
+
+for stream_name in "${sparse_graphs[@]}"
+do
+  > ${out_file}
+  echo "size,samples" > ${out_file}
+  ${exec_dir}/spanning_forest_extract ${datasets_dir}/real_world/${stream_name} $workers $readers $num_trials $seed
+  mv ${out_file} $result_dir/exhaustive_${stream_name}.csv
+done
+
+for stream_name in "${ktree_graphs[@]}"
+do
+  > ${out_file}
+  echo "size,samples" > ${out_file}
+  ${exec_dir}/spanning_forest_extract ${datasets_dir}/ktree/${stream_name} $workers $readers $num_trials $seed
+  mv ${out_file} $result_dir/exhaustive_${stream_name}.csv
+done
+
+# Merge results
+awk -F, 'NR==1 {print; next} FNR==1 {next} {sum[$1]+=$2} END {for (size in sum) print size "," sum[size]}' $result_dir/exhaustive_*_stream_binary*.csv | awk 'NR==1; NR>1 {print | "sort -g"}' > $result_dir/exhaustive_sketch_sampling.csv
--- tools/spanning_forest_extract.cpp
+++ tools/spanning_forest_extract.cpp
@@ -52,24 +52,25 @@ class TempCCSketchAlg : public CCSketchAlg {
 
 // Main function that populates a graph and then tests how many SFs can be extracted from it
 int main(int argc, char **argv) {
-  if (argc < 3 || argc > 5) {
+  if (argc < 5 || argc > 6) {
     std::cout << "ERROR: Incorrect number of arguments!" << std::endl;
-    std::cout << "Arguments: graph_stream trials [seed]" << std::endl;
+    std::cout << "Arguments: graph_stream graph_workers reader_threads trials [seed]" << std::endl;
     std::cout << "\"graph_stream\" must be a BinaryFileStream." << std::endl;
     std::cout << "Optionally specify a seed, otherwise one is chosen randomly" << std::endl;
     exit(EXIT_FAILURE);
   }
 
   std::string stream_file = argv[1];
-  size_t trials = std::stol(argv[2]);
+  int num_threads = std::atoi(argv[2]);
+  int reader_threads = std::atoi(argv[3]);
+  size_t trials = std::stol(argv[4]);
   size_t seed = get_seed();
-  if (argc == 5) {
-    seed = std::stol(argv[3]);
+  if (argc == 6) {
+    seed = std::stol(argv[5]);
   }
 
-  size_t num_threads = 40;
-  size_t reader_threads = 8;
   std::vector<size_t> rounds_required;
+  std::vector<double> rounds_factor;
   std::mt19937_64 seed_gen(seed);
 
   node_id_t num_vertices;
@@ -80,6 +81,7 @@ int main(int argc, char **argv) {
     num_updates  = stream.edges();
   }
   size_t vertex_power = ceil(log2(num_vertices));
+  size_t default_samples = Sketch::calc_cc_samples(num_vertices, 1);
   size_t errors = 0;
   size_t empty = 0;
   std::cout << "Extracting " << vertex_power << " Spanning Forests from: " << stream_file << std::endl;
@@ -92,7 +94,7 @@ int main(int argc, char **argv) {
     std::cout << "Trial #" << trial << std::endl;
 
     auto driver_config = DriverConfiguration().gutter_sys(CACHETREE).worker_threads(num_threads);
-    auto cc_config = CCAlgConfiguration().sketches_factor(1.33);
+    auto cc_config = CCAlgConfiguration().sketches_factor(1.6);
     TempCCSketchAlg cc_alg{num_vertices, seed_gen(), cc_config};
     GraphSketchDriver<TempCCSketchAlg> driver{&cc_alg, &stream, driver_config, reader_threads};
 
@@ -108,6 +110,10 @@ int main(int argc, char **argv) {
     rounds_required[cc_alg.last_query_rounds] += 1;
   }
 
+  for (size_t i = 0; i < rounds_required.size(); i++) {
+    rounds_factor.push_back((double)i / default_samples);
+  }
+
   std::cout << std::endl;
   std::cout << "ERRORS = " << errors << std::endl;
   std::cout << "EMPTY = " << empty << std::endl;
@@ -115,8 +121,22 @@ int main(int argc, char **argv) {
   auto stats = calc_stats(rounds_required);
   std::cout << "avg = " << stats.first << " std dev = " << stats.second << std::endl;
 
-  std::ofstream out("runtime_results.csv", std::ios_base::out | std::ios_base::app);
+  std::vector<double> sketch_sizes = {0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6};
+  std::vector<double> num_samples(sketch_sizes.size());
+
   for (size_t i = 0; i < rounds_required.size(); i++) {
-    out << i << ", " << rounds_required[i] << std::endl;
+    if (rounds_required[i] == 0) continue;
+
+    double num_factor = rounds_factor[i];
+
+    for (size_t j = 0; j < sketch_sizes.size() - 1; j++) {
+      if ((num_factor > sketch_sizes[j]) && (num_factor <= sketch_sizes[j + 1])) {
+        num_samples[j] += rounds_required[i];
+      }
+    }
+  }
+  std::ofstream out("runtime_results.csv", std::ios_base::out | std::ios_base::app);
+  for (size_t i = 0; i < sketch_sizes.size(); i++) {
+    out << sketch_sizes[i] << "," << num_samples[i] << std::endl;
   }
 }
